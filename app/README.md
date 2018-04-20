5a1afe1a807b79d34f339d01330457a98148e65e
Ivar Prudnikov <ivar.prudnikov@gmail.com>
2018-04-20 10:42:17 +0100

    flask prediction app in progress

--- app/README.md
+++ app/README.md
@@ -11,6 +11,9 @@ https://stackoverflow.com/a/41274348/906265
 - Create environment:
 `$ conda env create -f conda_requirements.yml`
 
+- Activate environment:
+`source activate face-app`
+
 ## Run app
 
 `gunicorn -w 4 -b 0.0.0.0:5000 -k gevent app:app`
--- app/app.py
+++ app/app.py
@@ -1,7 +1,34 @@
-from flask import Flask, render_template
+import io
 import os
 
+import tensorflow as tf
+from PIL import Image
+from flask import Flask, render_template, request, send_file
+from keras.models import model_from_json
+
+"""
+Some helping material:
+http://flask.pocoo.org/docs/0.12/
+https://github.com/moinudeen/digit-recognizer-flask-cnn
+https://github.com/llSourcell/how_to_deploy_a_keras_model_to_production
+https://github.com/ssola/python-flask-microservice
+https://github.com/leotok/pokedex-as-it-should-be
+https://www.pyimagesearch.com/2018/02/05/deep-learning-production-keras-redis-flask-apache/
+https://blog.hyperiondev.com/index.php/2018/02/01/deploy-machine-learning-models-flask-api/
+"""
+
 app = Flask(__name__)
+# limit max file size
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+allowed_extensions = set(['png', 'jpg', 'jpeg'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
+
+
+def convert_image_for_prediction(image_file):
+    pass
 
 
 @app.route('/')
@@ -9,6 +36,45 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'image' not in request.files:
+        return "No image part", 400
+    file = request.files['image']
+
+    if file.filename == '':
+        return "No selected image", 400
+
+    if not allowed_file(file.filename):
+        return 'Only {} types are allowed'.format(allowed_extensions), 400
+
+    # return file.content_type
+    send_file(Image.open(io.BytesIO(file.read())), mimetype=file.content_type)
+
+
+def load_model():
+    json_file = open('model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    print("Model loaded loaded from json")
+
+    # load woeights into new model
+    loaded_model.load_weights("model.h5")
+    print("Model weights loaded from disk")
+
+    # compile and evaluate loaded model
+    loaded_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    print("Model compiled")
+
+    graph = tf.get_default_graph()
+    print("Tensorflow graph obtained")
+
+    return loaded_model, graph
+
+
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 8080))
+
+    print("Start application")
+    app.run(host='0.0.0.0', port=port, debug=True)
